dcdb8495b21c667e09fe0c134a51c8529ab0f72c
Dave <157144137+vox45@users.noreply.github.com>
2024-01-25 18:46:53 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+//    public static void task1() {
+//        int current_value = 1;
+//        int print_times = 55;
+//        for (int i = 0;i < print_times ; i++) {
+//            System.out.println(current_value);
+//            current_value += 2;
+//        }
+//    }
+
+    //      public static void task2() {
+//          for (int i = 90; i > 0; i -= 5) {
+//              System.out.println(i);
+//          }
+//      }
+//        public static void task3() {
+//
+//        int number = 2;
+//        for (int i = 0;i < 20; i++) {
+//            System.out.println(number);
+//            number *= 2;
+//                }
+//        }
+//          public static void task4() {
+//              int[] numbers = {-29, -28, -27, -26, -25, -24, -23, -22, -21, -20,
+//                      -19, -18, -17, -16, -15, -14, -13, -12, -11, -10,
+//                      -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3,
+//                      4, 5, 6, 7, 8, 9, 10};
+//              for (int i = 0; i < numbers.length; i++) {
+//                  if (numbers[i] % 5 == 0) {
+//                    System.out.println(numbers[i]);
+//                  }
+//              }
+//
+//          }
+//    public static void task5() {
+//        int sum = 0;
+//        int evenCount = 0;
+//        for (int i = 50; i < 150; i++) {
+//            sum += i;
+//
+//            if (i % 2 == 0) {
+//                evenCount++;
+//            }
+//        }
+//        System.out.println(sum);
+//        System.out.println(evenCount);
+//
+//    }
+
+}
